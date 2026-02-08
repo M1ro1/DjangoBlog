b592ea7a3cd2ed0f086605bd0a7ea282f281e7f4
Your Name <maksym.lirko@gmail.com>
2026-02-08 20:55:33 +0200

    Add model and retriving data from db

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author','date','tags')
+    list_display = ('title','author','date')
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
 
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=150)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag)
