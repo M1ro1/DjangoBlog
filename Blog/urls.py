13d29267d562df74b462a8872386857bad5f0875
Your Name <maksym.lirko@gmail.com>
2026-02-13 18:21:47 +0200

    Add some changes

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path
+
+from .views import ReadLaterView
+
 urlpatterns = [
 
-    path('', views.starting_page, name='starting_page'),
-    path('posts/', views.posts, name='posts'),
-    path('posts/<slug:slug>/', views.post_info, name='post_info'),
+    path('', views.StartingPage.as_view(), name='starting_page'),
+    path('posts/', views.AllPostsView.as_view(), name='posts'),
+    path('posts/<slug:slug>/', views.SignalPostView.as_view(),
+         name='post_info'),
+    path('read-later', ReadLaterView.as_view(), name='read_later'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,97 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import *
-def starting_page(request):
-    posts_info = Post.objects.all().order_by("date")
-    print(posts_info[0].date)
-    return render(request, 'Blog/starting_page.html',{'post_info': posts_info})
+from django.views.generic import ListView
+from .forms import CommentForm
+from django.views import View
+from django.urls import reverse
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("date")
-    return render(request, 'Blog/all-posts.html', {'all_posts': all_posts})
+class StartingPage(ListView):
+    model = Post
+    template_name = 'Blog/starting_page.html'
+    ordering = ['-date']
+    context_object_name = 'post_info'
 
-def post_info(request, slug):
-    ind = Post.objects.get(slug=slug)
-    return render(request, 'Blog/post_info.html',{'post': ind})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+class AllPostsView(ListView):
+    model = Post
+    template_name = 'Blog/all-posts.html'
+    ordering = ['-date']
+    context_object_name = 'all_posts'
+
+class SignalPostView(View):
+    template_name = 'Blog/post_info.html'
+    model = Post
+
+    def get(self, request,slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id'),
+        }
+        return render(request, 'Blog/post_info.html',
+                      context)
+
+    def post(self, request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('post_info', args=[slug]))
+
+        context = {
+            'post': post,
+            'comment_form': comment_form,
+            'comments': post.comments.all().order_by('-id'),
+
+        }
+        return render(request, 'Blog/post_info.html', context)
+
+class ReadLaterView(View):
+    def get(self,request):
+        stored_posts = request.session.get('stored_posts')
+
+        context = {}
+        if not stored_posts:
+            context['posts'] = []
+            context['has_posts'] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context['posts'] = posts
+            context['has_posts'] = True
+
+        return render(request, 'Blog/stored_posts.html', context)
+
+    def post(self,request):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is None:
+            stored_posts = []
+
+        try:
+            post_id = int(request.POST.get('post_id'))
+        except (TypeError, ValueError):
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+        action = request.POST.get('action', 'add')
+
+        if action == 'remove':
+            if post_id in stored_posts:
+                stored_posts.remove(post_id)
+                request.session['stored_posts'] = stored_posts
+            return HttpResponseRedirect(reverse('read_later'))
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session['stored_posts'] = stored_posts
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
